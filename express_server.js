d623fcb978142eeb1df2d21c61175aaf8b1f09f0
Gabriel Cadiz <gabecadiz@hotmail.com>
2019-02-12 23:49:48 +0000

    updated express server so that shortURL - longURL key-value pair are saved to the urlDatabase

--- express_server.js
+++ express_server.js
@@ -31,8 +31,9 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  urlDatabase[generateRandomString()] = req.body.longURL; // Log the POST request body to the console
+  console.log(urlDatabase)
+  res.send("Ok");
 });
 
 app.get("/urls", (req, res) =>{
